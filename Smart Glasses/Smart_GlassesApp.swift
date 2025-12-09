990e85eaa21323b3053e29682a7e65fb1418d13a
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2025-12-09 10:30:24 -0800

    Initial Commit

---
+++ Smart Glasses/Smart_GlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_GlassesApp.swift
+//  Smart Glasses
+//
+//  Created by Alphonso Sensley II on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_GlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
